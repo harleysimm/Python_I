9ae6e5c195c8a30e54d957dabac8a8afc758cdd7
harleysimm <130719372+harleysimm@users.noreply.github.com>
2023-04-20 19:31:26 -0400

    first commit

---
+++ Clase-dos/hola.py
@@ -0,0 +1,17 @@
+print("Hola Soledad!!!")
+print("esto es una línea nueva")
+
+edad = 42
+sueldo = 800.000
+nombre = "Soledad"
+isStudent = True
+
+print(edad, sueldo, nombre, isStudent)
+
+
+numero_1 = 8
+numero_2 = 2
+numero_3 = 25
+resultado = ((numero_1 / numero_2) * numero_3)
+print(resultado)
+

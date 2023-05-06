3c3a7af1ca2f67caee928f619b9bdd89ccb01001
harleysimm <130719372+harleysimm@users.noreply.github.com>
2023-05-06 10:10:07 -0400

    cinco

--- ejercicios/ejercicio-cinco.py
+++ ejercicios/ejercicio-cinco.py
@@ -1,6 +1,6 @@
-cantidad = int(input("Introduzca la cantidad de dinero a invertir: "))
-intereses = float(input("Introduzca el interés: "))
-años = int(input("Introduzca el número de años: "))
+cantidad = int(input("Ingrese la cantidad de dinero a invertir: "))
+intereses = float(input("Ingrese el interés: "))
+anios = int(input("Ingrese el número de años: "))
 
-capital = cantidad * años * intereses
-print(f"El capital obtenido en la inversión es {int(capital)}")
+capital = cantidad * (1 + (intereses/100))**anios
+print(f"El capital obtenido en la inversión es {round(capital, 2)} dolares")
